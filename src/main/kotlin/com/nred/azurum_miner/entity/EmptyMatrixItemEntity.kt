78ccd6630977ef854e8ced3ae75f28501099faa3
Nred <oilsafats@gmail.com>
2024-12-28 18:10:54 -0500

    Fixed dimensional matrix exploit and added more info to jei / emi

--- src/main/kotlin/com/nred/azurum_miner/entity/EmptyMatrixItemEntity.kt
+++ src/main/kotlin/com/nred/azurum_miner/entity/EmptyMatrixItemEntity.kt
@@ -3,6 +3,7 @@ package com.nred.azurum_miner.entity
 import com.nred.azurum_miner.item.ModItems
 import com.nred.azurum_miner.item.ModItems.EMPTY_DIMENSIONAL_MATRIX_TYPE
 import net.minecraft.core.BlockPos
+import net.minecraft.core.Direction
 import net.minecraft.core.particles.ParticleTypes
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.server.level.ServerLevel
@@ -13,8 +14,10 @@ import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.NetherPortalBlock
 import net.minecraft.world.level.block.Portal
+import net.minecraft.world.level.levelgen.structure.BoundingBox
 import net.minecraft.world.phys.AABB
 import net.minecraft.world.phys.Vec3
+import thedarkcolour.kotlinforforge.neoforge.forge.vectorutil.v3d.toVec3
 import thedarkcolour.kotlinforforge.neoforge.forge.vectorutil.v3d.toVec3i
 import kotlin.random.Random
 
@@ -23,10 +26,12 @@ class EmptyMatrixItemEntity : ItemEntity {
     var dest: Vec3? = null
 
     constructor(entityType: EntityType<EmptyMatrixItemEntity>, level: Level) : super(entityType, level)
-    constructor(tag: CompoundTag, level: Level) : this(EMPTY_DIMENSIONAL_MATRIX_TYPE.get(), level) {
+    constructor(tag: CompoundTag, level: Level) : this(tag, level, 1)
+    constructor(tag: CompoundTag, level: Level, count: Int) : this(EMPTY_DIMENSIONAL_MATRIX_TYPE.get(), level) {
         this.load(tag)
         this.setPickUpDelay(60)
         this.setUnlimitedLifetime()
+        this.item.count = count
     }
 
     override fun getType(): EntityType<*> {
@@ -37,7 +42,8 @@ class EmptyMatrixItemEntity : ItemEntity {
         super.addAdditionalSaveData(compound)
 
         compound.putInt("matrixFill", fill)
-        compound.put("matrixDest", this.newDoubleList(dest!!.x, dest!!.y, dest!!.z))
+        if (this.dest != null)
+            compound.put("matrixDest", this.newDoubleList(dest!!.x, dest!!.y, dest!!.z))
     }
 
     override fun readAdditionalSaveData(compound: CompoundTag) {
@@ -50,6 +56,7 @@ class EmptyMatrixItemEntity : ItemEntity {
     }
 
     override fun tick() {
+        super.tick()
         if (this.dest != null) {
             if (this.fill < 4) {
                 this.lerpPositionAndRotationStep(4, this.dest!!.x, this.dest!!.y, this.dest!!.z, this.rotationVector.y.toDouble(), this.rotationVector.x.toDouble())
@@ -76,16 +83,62 @@ class EmptyMatrixItemEntity : ItemEntity {
                 this.setDefaultPickUpDelay()
                 this.dest = null
             }
+        } else if (!level().isClientSide) {
+            if (this.item.count < 64) { // Merge stacks
+                for (itemEntity in this.level().getEntitiesOfClass(this::class.java, this.boundingBox.inflate(0.5, 0.0, 0.5), { e -> e != this && areMergable(this.item, e.item) })) {
+                    if (areMergable(this.item, itemEntity.item) && itemEntity.dest == null) {
+                        this.item = merge(this.item, itemEntity.item, this.item.count + itemEntity.item.count)
+                        itemEntity.discard()
+                    }
+                }
+            }
+        }
+    }
+
+    fun findPortalBounds(pos: BlockPos, axis: Direction.Axis): AABB {
+        var top = pos.toVec3().toVec3i()
+        var bottom = pos.toVec3().toVec3i()
+
+        while (level().getBlockState(BlockPos(top.above())).block is NetherPortalBlock) {
+            top = top.above()
         }
 
-        super.tick()
+        while (level().getBlockState(BlockPos(bottom.below())).block is NetherPortalBlock) {
+            bottom = bottom.below()
+        }
+
+        var temp = top
+        do {
+            top = temp
+            temp = top.relative(axis, 1)
+        } while (level().getBlockState(BlockPos(temp)).block is NetherPortalBlock)
+
+        temp = bottom
+        do {
+            bottom = temp
+            temp = bottom.relative(axis, -1)
+        } while (level().getBlockState(BlockPos(temp)).block is NetherPortalBlock)
+
+        return AABB.of(BoundingBox.fromCorners(top, bottom))
     }
 
     override fun setAsInsidePortal(portal: Portal, pos: BlockPos) {
         if (level().isClientSide || fill != 0 || portal !is NetherPortalBlock) return
-        val bounds = this.makeBoundingBox().move(this.x, this.y, pos.center.z)
 
-        dest = if (level().getBlockState(pos).getValue(portal.stateDefinition.getProperty("axis")) == "z")
+        val axis = level().getBlockState(pos).getValue(portal.stateDefinition.getProperty("axis")) as Direction.Axis
+        val portalBounds = findPortalBounds(pos, axis)
+        val found = level().getEntitiesOfClass(this::class.java, portalBounds)
+
+        if (this.item.count > 1 || found.filter { it.item.count == 1 }.size > 1) { // Make sure only one is in the portal
+            this.deltaMovement = Vec3(if (axis == Direction.Axis.Z) this.deltaMovement.reverse().scale(0.5).x else this.deltaMovement.x, 0.0, if (axis == Direction.Axis.X) this.deltaMovement.reverse().scale(0.5).z else this.deltaMovement.z)
+            level().playSeededSound(null, this.x, this.y, this.z, PLAYER_TELEPORT, this.soundSource, 0.5f, 0.1f, -198679135428719823)
+            this.fill = 1
+            this.setDefaultPickUpDelay()
+            return
+        }
+
+        val bounds = this.makeBoundingBox().move(this.x, this.y, pos.center.z)
+        dest = if (axis == Direction.Axis.Z)
             Vec3(AABB.ofSize(pos.center, bounds.xsize, bounds.ysize, bounds.zsize).center.x(), this.y, this.z)
         else
             Vec3(this.x, this.y, AABB.ofSize(pos.center, bounds.xsize, bounds.ysize, bounds.zsize).center.z)
