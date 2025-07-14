3644ed49857909a76cbf47e2f9fe9cc95cd41360
Nred <oilsafats@gmail.com>
2025-07-14 15:22:04 -0400

    Added tooltip to Palestium Pickaxe

--- src/main/kotlin/com/nred/azurum_miner/item/PalestiumPickaxe.kt
+++ src/main/kotlin/com/nred/azurum_miner/item/PalestiumPickaxe.kt
@@ -1,13 +1,16 @@
 package com.nred.azurum_miner.item
 
 import com.nred.azurum_miner.AzurumMiner.CONFIG
+import net.minecraft.ChatFormatting
 import net.minecraft.core.BlockPos
+import net.minecraft.network.chat.Component
 import net.minecraft.world.effect.MobEffectInstance
 import net.minecraft.world.effect.MobEffects
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.PickaxeItem
 import net.minecraft.world.item.Tier
+import net.minecraft.world.item.TooltipFlag
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.state.BlockState
 
@@ -18,5 +21,9 @@ class PalestiumPickaxe(tier: Tier, properties: Properties) : PickaxeItem(tier, p
             miningEntity.addEffect(MobEffectInstance(MobEffects.DIG_SPEED, CONFIG.getInt("palestium_pickaxe.hasteOnMine"), 0))
         }
         return rtn
+    }
+
+    override fun appendHoverText(stack: ItemStack, context: TooltipContext, tooltipComponents: MutableList<Component>, tooltipFlag: TooltipFlag) {
+        tooltipComponents.add(Component.translatable("tooltip.azurum_miner.palestium_pickaxe").withStyle(ChatFormatting.GRAY, ChatFormatting.ITALIC))
     }
 }
